5664874195b25a6fd7e7ff6ea40d9327f5520207
masayo-ikeda <masayon7777777@yahoo.co.jp>
2021-11-18 11:41:35 +0900

    提出No7.商品詳細表示機能/実装条件シート香川県
    提出

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,48 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  #authenticate_user!メソッドはログインに必要なデバイス
+  # プロトスペース
+  # before_action :set_prototype, except: [:index, :new, :create]
+  #deviseのメソッド
+  #ユーザがログインしているかどうかを確認し、ログインしていない場合はユーザをログインページにリダイレクトする
+  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.order("created_at DESC")
   end
 
   def new
-    @item = Item.new
+    @items = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    @items = Item.new(item_params)
+    if @items.save
       redirect_to root_path
     else
       render :new
     end
   end
 
+  def show
+    @items = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+  end
   private
 
   def item_params
     params.require(:item).permit(:item, :info, :category_id, :image, :rank_id, :postage_id, :area_id, :scheduled_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @items = Item.find(params[:id])
   end
 end
