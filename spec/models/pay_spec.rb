2681107ad7c25b609de1e466a8e8570343c536a5
masayo-ikeda <masayon7777777@yahoo.co.jp>
2021-11-28 17:07:52 +0900

    再提出
    修正完了

--- spec/models/pay_spec.rb
+++ spec/models/pay_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe Pay, type: :model do
     before do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
-      # order = FactoryBot.create(:order)
-      # @pay = FactoryBot.build(:pay, user_id: user.id, item_id: item.id, order_id: order.id)
       @pay = FactoryBot.build(:pay, user_id: user.id, item_id: item.id)
     end
   
